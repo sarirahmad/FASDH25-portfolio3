03ecb10d235cf135479c6ba53a19a353cc0c4dd8
naveerataj <naveerataj@gmail.com>
2025-05-30 00:34:14 +0500

    I have created a new folder for Scripts of our group in the FASDH25-portfolio3 folder and have added my python code file there.

---
+++ Scripts/SCRIPT PROJECT 3 naveera.py
@@ -0,0 +1,12 @@
+# importing the pandas and os 
+import pandas as pd
+import os 
+#Seting the path to the CSV file
+import os
+print(os.path.exists(r'C:\Users\HP\Downloads\FASDH25-portfolio3\data\dataframes\length-year-month.csv'))
+
+# Step 2: Load the CSV from the Repo
+df = pd.read_csv('your_filename.csv')
+# step 3: Finding Metadata trends and finding descriptive statistics in the CSV dataset
+
+# Step 4: 
